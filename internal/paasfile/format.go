0d1d00601589e02405c0a49d77cfb92b677b1d6d
Devotional Phoenix <158271509+devotional-phoenix-97@users.noreply.github.com>
2026-04-13 19:20:53 +0200

    generating improved

--- internal/paasfile/format.go
+++ internal/paasfile/format.go
@@ -9,17 +9,32 @@ type Format uint
 const (
 	UnknownFormat Format = iota
 	AutoFormat
-	PreserveFormat
+	DefaultFormat
 	JSONFormat
+	PreserveFormat
+	RawFormat
 	YAMLFormat
 )
 
 var fileFormatString = map[Format]string{
 	AutoFormat:     "auto",
+	DefaultFormat:  "default",
 	JSONFormat:     "json",
 	YAMLFormat:     "yaml",
 	PreserveFormat: "preserve",
+	RawFormat:      "raw",
 	UnknownFormat:  "unknown",
+}
+
+var stringFileFormat = map[string]Format{
+	"auto":     AutoFormat,
+	"default":  DefaultFormat,
+	"":         DefaultFormat,
+	"json":     JSONFormat,
+	"yaml":     YAMLFormat,
+	"preserve": PreserveFormat,
+	"raw":      RawFormat,
+	"unknown":  UnknownFormat,
 }
 
 // Returns a string representation of fileFormat, if known.
@@ -32,10 +47,8 @@ func (ff Format) String() string {
 
 // FormatFromString can be used to get the corresponding format for a string value
 func FormatFromString(sFormat string) (Format, error) {
-	for f, s := range fileFormatString {
-		if s == sFormat {
-			return f, nil
-		}
+	if format, exists := stringFileFormat[sFormat]; exists {
+		return format, nil
 	}
 	return UnknownFormat, fmt.Errorf("unknown file format %s", sFormat)
 }
