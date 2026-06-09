2d60b5ccfbcdf8b736603191777120b26819e927
westerlypeppermint <259304337+westerlypeppermint@users.noreply.github.com>
2026-06-09 22:50:44 +0200

    fix: make sure the current private key can be properly identified (#213)
    * make sure the current private key can be properly identified
    
    Signed-off-by: Thomas Vijgen <ta.vijgen@belastingdienst.nl>
    
    * fixed e2e
    
    * added tests
    
    * linting issue; fix bump linter version
    
    ---------
    
    Signed-off-by: Thomas Vijgen <ta.vijgen@belastingdienst.nl>

--- pkg/crypt/private_keys.go
+++ pkg/crypt/private_keys.go
@@ -17,9 +17,11 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/belastingdienst/opr-paas-cli/v2/internal/utils"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -43,6 +45,7 @@ func NewPrivateKeysFromFiles(privateKeyPaths []string) (PrivateKeys, error) {
 	}
 
 	for _, file := range files {
+		logrus.Debugf("reading file %v", file)
 		if pk, err = NewPrivateKeyFromFile(file); err != nil {
 			return nil, fmt.Errorf("invalid private key file %s", file)
 		}
@@ -81,8 +84,11 @@ func (pks PrivateKeys) PublicKey() (*rsa.PublicKey, error) {
 			return &pk.privateKey.PublicKey, nil
 		}
 	}
-	if pk, exists := pks["current"]; exists {
-		return &pk.privateKey.PublicKey, nil
+
+	for _, pk := range pks {
+		if pk.isCurrent {
+			return &pk.privateKey.PublicKey, nil
+		}
 	}
 	return nil, errors.New("cannot get Public key from multiple keys unless there is a 'current' key")
 }
@@ -123,6 +129,7 @@ type PrivateKey struct {
 	fingerprint   string
 	privateKeyPem []byte
 	privateKey    *rsa.PrivateKey
+	isCurrent     bool
 }
 
 // GetID returns an ID generated from public key has and date of insertion
@@ -173,11 +180,18 @@ func NewPrivateKeyFromPem(privateKeyPath string, privateKeyPem []byte) (*Private
 			return nil, errors.New("geen RSA private key gevonden in PKCS8 block")
 		}
 	}
+
+	isCurrent := filepath.Base(privateKeyPath) == "current"
+	if isCurrent {
+		isCurrent = true
+	}
+
 	return &PrivateKey{
 		timestamp:     time.Now(),
 		fingerprint:   fmt.Sprintf("%x", sha256.Sum256(key.N.Bytes())),
 		privateKeyPem: privateKeyPem,
 		privateKey:    key,
+		isCurrent:     isCurrent,
 	}, nil
 }
 
---
+++ pkg/crypt/private_keys_test.go
@@ -0,0 +1,119 @@
+package crypt
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PrivateKeys", func() {
+	Context("getting PublicKey from private key(s)", func() {
+		When("when multiple private keys are present and one is the 'current' key", func() {
+			It("it should return the public key belonging to the 'current' private key", func() {
+				// generate private keys
+				privateFile1, err := os.CreateTemp("", "private1")
+				Expect(err).NotTo(HaveOccurred())
+				defer os.Remove(privateFile1.Name()) // clean up
+
+				privateFile2, err := os.CreateTemp("", "private2")
+				Expect(err).NotTo(HaveOccurred())
+				defer os.Remove(privateFile2.Name()) // clean up
+
+				// write private keys to files
+				pk1, err := GeneratePrivateKey()
+				Expect(err).NotTo(HaveOccurred())
+				err = pk1.WritePrivateKey(privateFile1.Name())
+				Expect(err).NotTo(HaveOccurred())
+
+				pk2, err := GeneratePrivateKey()
+				Expect(err).NotTo(HaveOccurred())
+				err = pk2.WritePrivateKey(privateFile2.Name())
+				Expect(err).NotTo(HaveOccurred())
+				pk2.isCurrent = true
+
+				privateKeys := PrivateKeys{
+					"pk1": pk1,
+					"pk2": pk2,
+				}
+
+				// get public key from private keys
+				key, err := privateKeys.PublicKey()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(key).To(Equal(&pk2.privateKey.PublicKey))
+			})
+		})
+		When("when multiple private keys are present and there is no 'current' key", func() {
+			It("it should return an error", func() {
+				// generate private keys
+				privateFile1, err := os.CreateTemp("", "private1")
+				Expect(err).NotTo(HaveOccurred())
+				defer os.Remove(privateFile1.Name()) // clean up
+
+				privateFile2, err := os.CreateTemp("", "private2")
+				Expect(err).NotTo(HaveOccurred())
+				defer os.Remove(privateFile2.Name()) // clean up
+
+				// write private keys to files
+				pk1, err := GeneratePrivateKey()
+				Expect(err).NotTo(HaveOccurred())
+
+				err = pk1.WritePrivateKey(privateFile1.Name())
+				Expect(err).NotTo(HaveOccurred())
+
+				pk2, err := GeneratePrivateKey()
+				Expect(err).NotTo(HaveOccurred())
+
+				err = pk2.WritePrivateKey(privateFile2.Name())
+				Expect(err).NotTo(HaveOccurred())
+
+				privateKeys := PrivateKeys{
+					"pk1": pk1,
+					"pk2": pk2,
+				}
+
+				// get public key from private keys
+				key, err := privateKeys.PublicKey()
+
+				Expect(err).To(HaveOccurred())
+				Expect(key).To(BeNil())
+			})
+		})
+		When("when one private key is present", func() {
+			It("it should return the public key belonging to the private key", func() {
+				// generate private key
+				privateFile1, err := os.CreateTemp("", "private1")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(err).NotTo(HaveOccurred())
+				defer os.Remove(privateFile1.Name()) // clean up
+
+				// write private key to file
+				pk1, err := GeneratePrivateKey()
+				Expect(err).NotTo(HaveOccurred())
+
+				err = pk1.WritePrivateKey(privateFile1.Name())
+				Expect(err).NotTo(HaveOccurred())
+
+				// get public key from private keys
+				privateKeys := PrivateKeys{
+					"pk1": pk1,
+				}
+
+				key, err := privateKeys.PublicKey()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(key).To(Equal(&pk1.privateKey.PublicKey))
+			})
+		})
+		When("no private keys are present", func() {
+			It("it should return an error", func() {
+				// no private keys present
+				privateKeys := PrivateKeys{}
+
+				key, err := privateKeys.PublicKey()
+				Expect(err).To(HaveOccurred())
+				Expect(key).To(BeNil())
+			})
+		})
+	})
+})
